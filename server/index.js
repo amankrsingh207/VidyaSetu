d7812a49f86352dc73a8605c543b5ac9c9d6a3a7
Alok Patel <alokpatelmnnit@gmail.com>
2025-07-29 15:41:47 +0530

    error fixed

--- server/index.js
+++ server/index.js
@@ -25,8 +25,12 @@ const server = http.createServer(app); // ✅ Use HTTP server for socket.io
 // Setup Socket.IO server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // ✅ Allow frontend to connect
+    origin: [
+      "http://localhost:5173",
+      "https://vidya-setu-frontend-ruddy.vercel.app",
+    ],
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
@@ -38,7 +42,11 @@ app.use((req, res, next) => {
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/uploads", express.static("uploads"));
 
 // Routes
